85da004595c325073acbf20d125f8382de99690d
Milos Gavranovic <mgavranovic7@gmail.com>
2026-01-23 09:53:05 +0100

    Work on About component

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,10 +1,33 @@
 function About() {
   return (
     <div className="flex h-dvh border-2  border-blue-600 items-center justify-center">
-      <div className="flex justify-between w-50">
-        <p>ABOUT ME</p>
-        <p>Image</p>
+      <div className="w-3/5 p-5 text-justify">
+        <img
+          src="../../../src/assets/avatar-placeholder.png"
+          alt="avatar-placeholdere"
+          className="w-30 h-30 float-left mr-5 mb-5"
+        />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed error
+          asperiores quasi est esse laboriosam accusantium, similique
+          exercitationem veniam, repudiandae quas quo in quidem ab delectus, ut
+          qui sunt itaque. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Repudiandae numquam debitis, unde ut et quae delectus. Libero
+          iure quas nobis omnis quaerat sit aliquam, repellendus non impedit
+          modi cumque possimus! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Hic deserunt itaque, illum exercitationem
+          necessitatibus eligendi ad suscipit eius sed cumque, minus eos totam
+          aut at maiores saepe impedit aliquid beatae. Lorem ipsum dolor sit,
+          amet consectetur adipisicing elit. Blanditiis perspiciatis earum,
+          laboriosam quidem minus obcaecati possimus nisi voluptate, autem
+          magnam velit nesciunt distinctio neque! Alias quaerat magni ea eos!
+          Cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Repellendus vitae, at accusantium eaque id officia aliquam est, hic
+          veritatis animi iure, culpa fugit a in? Reprehenderit quod
+          consequuntur ullam incidunt!
+        </p>
       </div>
+      <div className="w-2/5">design</div>
     </div>
   );
 }
