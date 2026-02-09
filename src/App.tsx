c29cafa5601472c738d6ff0c6be6071f59f41503
Milos Gavranovic <mgavranovic7@gmail.com>
2026-02-09 13:59:54 +0100

    Add animated cursor/Issue: offset to the actual cursor loc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Cursor from "./ui/Cursor";
 import Navbar from "./components/navbar/Navbar";
 import Section from "./components/sections/Section";
 import { SectionType } from "./constants";
@@ -6,6 +7,7 @@ import { SectionType } from "./constants";
 function App() {
   return (
     <>
+      <Cursor animated={false}></Cursor>
       <Navbar></Navbar>
       <div>
         <Section type={SectionType.ABOUT}></Section>
---
+++ src/ui/Cursor.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+
+type CursorProps = {
+  animated: boolean;
+};
+
+function Cursor({ animated }: CursorProps) {
+  useEffect(() => {
+    const cursor: HTMLElement | null =
+      document.getElementById("animated-cursor");
+
+    if (!cursor) return;
+
+    const move = (e: MouseEvent) => {
+      cursor.style.setProperty("--x", `${e.clientX}px`);
+      cursor.style.setProperty("--y", `${e.clientY}px`);
+    };
+
+    window.addEventListener("mousemove", move);
+    return () => window.removeEventListener("mousemove", move);
+  }, []);
+
+  return (
+    <div
+      id="animated-cursor"
+      className={`fixed
+        w-16 h-full
+        pointer-events-none
+        bg-no-repeat
+        ${
+          animated
+            ? "bg-[url('./assets/cursor/cursor-hover-sheet.png')] animate-cursor"
+            : "bg-[url('./assets/cursor/cursor-static.webp')]"
+        }`}
+    ></div>
+  );
+}
+
+export default Cursor;
